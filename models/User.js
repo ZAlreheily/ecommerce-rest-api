732bb635fe94557c39880e3541aae9e11679b944
Ziad Alreheily <ziyad1913@gmail.com>
2023-01-26 18:32:05 +0300

    Add password to User Schema

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     email: { type: String, required: true },
+    password:{type:String,require:true},
     addresses: [{ country: String, city: String, street: String, zipcode: Number }],
     createDate: { default: Date.now(), immutable: true }
 })
