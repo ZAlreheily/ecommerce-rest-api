e0bf735be17bdf5f44018773214d1cc9c8ee3130
Ziad Alreheily <ziyad1913@gmail.com>
2023-01-26 17:51:21 +0300

    Added createDate to User schema

--- models/User.js
+++ models/User.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName: String,
-    lastName:String,
+    lastName: String,
     email: { type: String, required: true },
-    address: { country: String, city: String, street: String, zipcode: Number}
+    address: { country: String, city: String, street: String, zipcode: Number },
+    createDate: { default: Date.now() }
 })
 
 module.exports = userSchema.model("User", userSchema);
