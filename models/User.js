1fa1d1f68a401b7748e0ff1e3ce912d22ec9d9e2
Ziad Alreheily <ziyad1913@gmail.com>
2023-01-26 18:20:00 +0300

    set createdDate to immutable in User schema

--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     email: { type: String, required: true },
-    address: { country: String, city: String, street: String, zipcode: Number },
-    createDate: { default: Date.now() }
+    addresses: [{ country: String, city: String, street: String, zipcode: Number }],
+    createDate: { default: Date.now(), immutable: true }
 })
 
 module.exports = userSchema.model("User", userSchema);
