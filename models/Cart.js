97e6e69bec0e435b6891d92d82917c2b02419ac8
Ziad Alreheily <ziyad1913@gmail.com>
2023-01-30 21:21:49 +0300

    add addQuantity() to Cart.js

--- models/Cart.js
+++ models/Cart.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
     products: [{
-        itemId: { type: mongoose.SchemaTypes.ObjectId, ref: "product" },
+        itemId: { type: mongoose.SchemaTypes.ObjectId, ref: "Product" },
         quantity: { type: Number, default: 1, required: true }
     }]
 });
+
+// cartSchema.method.addQuantity(function(quantity, productId){
+//     let tmpQuantity = this.quantity + quantity;
+//     let product = this.products.find(obj => obj.itemId == productId)
+//     if (product == undefined){
+//         new Error("No product with that id is in card");
+//     }
+//     if (tmpQuantity > ){
+
+//     }
+//     product.quantity = tmpQuantity;
+// })
 
 module.exports = mongoose.model("Cart", cartSchema);
