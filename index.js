acbcfa3fbd4f9da21104611408fe821b39f7d8df
Ziad Alreheily <ziyad1913@gmail.com>
2023-01-30 21:33:23 +0300

    import and use routes in index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const app = express();
-
-app.use(express.json());
+const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
+const cartRoute = require("./routes/cart");
+const authRoute = require("./routes/auth");
 
 const PORT = process.env.PORT || 3000;
 
-const users = []
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Success."))
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.get("/", () => {
-    console.log("hello")
-})
+  app.use(express.json());
 
-
+  app.use("/api/auth", authRoute);
+  app.use("/api/users", userRoute);
+  app.use("/api/products", productRoute);
+  app.use("/api/orders", orderRoute);
+  app.use("/api/carts", cartRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is Running on Port ${PORT}`)
