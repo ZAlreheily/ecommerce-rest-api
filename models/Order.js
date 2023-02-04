142193d0982e234ebc7593c001bb0eca3154e538
Ziad Alreheily <ziyad1913@gmail.com>
2023-02-04 15:28:30 +0300

    optimized Order schema

--- models/Order.js
+++ models/Order.js
@@ -1,27 +1,26 @@
 const mongoose = require("mongoose");
 
-const orderItemSchema = mongoose.Schema({
-    item: { type: mongoose.SchemaTypes.ObjectId, ref: "Product", required: true },
-    price: { type: Number, required: true, immutable: true },
-    quantity: { type: Number, required: true, default: 1 }
-});
-
 const orderSchema = new mongoose.Schema({
+
     user: { type: mongoose.SchemaTypes.ObjectId, ref: "User", required: true },
     // adress: {type:mongoose.SchemaTypes.ObjectId,ref:"Adress",required:true},
     createDate: { default: Date.now(), immutable: true },
-    items: [orderItemSchema],
-    status: { type: String, defult: "in progress" }
+    items: [{
+        item: { type: mongoose.SchemaTypes.ObjectId, ref: "Product", required: true },
+        price: { type: Number, required: true, immutable: true },
+        quantity: { type: Number, required: true, default: 1 }
+    }],
+    status: { type: String, default: "in progress" }
+
 });
 
-orderSchema.pre("save", function (next) {
+orderSchema.virtual("total").get(function () {
     let tmpTotal = 0
-    items.array.forEach(element => {
+    this.items.array.forEach(element => {
         tmpTotal += element.price * element.quantity;
     });
     this.total = tmpTotal
-    next()
-})
+});
 
 
 module.exports = mongoose.Schema("Order", orderSchema);
