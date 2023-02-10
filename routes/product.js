8537e9762792fb0a2dcd6459e3f040c2d87174da
Ziad Alreheily <ziyad1913@gmail.com>
2023-02-10 18:14:19 +0300

    implement products/:id get request

--- routes/product.js
+++ routes/product.js
@@ -1,31 +1,46 @@
 const router = require("express").Router();
 const Product = require("../models/Product");
+const mongoose = require("mongoose");
 
 router.get("/", async (req, res) => {
     const allProducts = await Product.find();
     res.status(200).json(allProducts);
-})
+});
 
-router.get("/:id", (req, res) => {
-    
-})
+router.get("/:id", async (req, res, next) => {
+    const productId = req.params.id;
+    const isValidId = mongoose.isValidObjectId(productId)
+
+    if (!isValidId) {
+        const err = new Error("ID is not valid.");
+        err.status = 400;
+        return next(err);
+    }
+
+    try {
+        const product = await Product.findById(productId);
+        res.status(200).json(product)
+    } catch (err) {
+        next(err)
+    }
+});
 
 router.post("/", async (req, res) => {
     const product = new Product(req.body);
-    try{
+    try {
         const savedProduct = await product.save();
-        res.status(200).json(savedProduct);
-    } catch(err) {
+        res.status(201).json(savedProduct);
+    } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 router.delete("/:id", (req, res) => {
-    
-})
+
+});
 
 router.put("/id", (req, res) => {
-    
-})
+
+});
 
 module.exports = router;
