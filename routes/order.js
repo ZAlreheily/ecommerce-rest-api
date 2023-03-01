2ebf35e33bb7eeada2c5232c3b1ff0f0ee0a421b
Ziad Alreheily <ziyad1913@gmail.com>
2023-03-01 22:09:28 +0300

    add get order by id route

--- routes/order.js
+++ routes/order.js
@@ -8,12 +8,29 @@ router.get('/', authMiddleware, async (req, res, next) => {
     res.status(200).json(orders);
 });
 
+router.get('/:orderID', authMiddleware, async (req, res, next) => {
+    const userID = req.user;
+    const { orderID } = req.body;
+    try {
+     const order = await Order.findOne({ __id: orderID });
+        if (userID !== order.user){
+            const err = new Error('You are not authorized to get order information.');
+            err.status = 403;
+            return next(err);
+        }
+    
+        res.status(200).json(order);
+    } catch (err){
+        next(err);
+    }
+
+});
+
 router.post('/', authMiddleware, async (req, res, next) => {
     const userID = req.user;
     const { address, items } = req.body;
-    console.log(address)
     try {
-        const newOrder = await Order.create({ user: userID, address:address, items:items });
+        const newOrder = await Order.create({ user: userID, address: address, items: items });
         res.status(200).json(newOrder);
     } catch (err) {
         const error = new Error('Information was not in correct format.')
